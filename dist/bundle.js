3f423d13719702fa65d32e36224ac1dddfeeca82
Rory Flint <roryflintphoto@gmail.com>
2020-08-06 11:29:07 +0100

    Render multiple markers on map

--- dist/bundle.js
+++ dist/bundle.js
@@ -95,15 +95,16 @@ __webpack_require__.r(__webpack_exports__);
 const testDOM = document.getElementById('test'); // window.onload = function test() {
 //   setInterval(() => {
 //     issLocation().then((res) => {
-//       writeToDom(res);
+//       console.log(res);
 //     });
 //   }, 2000);
 // };
-// function writeToDom(data) {
-//   const h3 = document.createElement('h3');
-//   h3.textContent = `Longitude: ${data.longitude}, Latitude: ${data.latitude}`;
-//   testDOM.append(h3);
-// }
+
+function writeToDom(data) {
+  const h3 = document.createElement('h3');
+  h3.textContent = `Longitude: ${data.longitude}, Latitude: ${data.latitude}`;
+  testDOM.append(h3);
+}
 
 /***/ }),
 /* 1 */
--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const testDOM = document.getElementById('test');
 // window.onload = function test() {
 //   setInterval(() => {
 //     issLocation().then((res) => {
-//       writeToDom(res);
+//       console.log(res);
 //     });
 //   }, 2000);
 // };
 
-// function writeToDom(data) {
-//   const h3 = document.createElement('h3');
-//   h3.textContent = `Longitude: ${data.longitude}, Latitude: ${data.latitude}`;
-//   testDOM.append(h3);
-// }
+function writeToDom(data) {
+  const h3 = document.createElement('h3');
+  h3.textContent = `Longitude: ${data.longitude}, Latitude: ${data.latitude}`;
+  testDOM.append(h3);
+}
